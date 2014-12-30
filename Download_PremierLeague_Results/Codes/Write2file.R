3bb304047e91ecd9643eeae2f95c98df324586fd
nimahraja <nimaradja@gmail.com>
2014-12-30 12:53:04 +0000

    minor update

--- Download_PremierLeague_Results/Codes/Write2file.R
+++ Download_PremierLeague_Results/Codes/Write2file.R
@@ -1,16 +1,16 @@
 write2file <- function(df, name, ...){
     
-    if (file.exists("data")) {
-        print("Warning! /data exists.")
+    if (file.exists("../Sample_Outputs")) {
+        print("Warning! ../Sample_Outputs exists.")
     }
     else {
-        dir.create("data")
+        dir.create("../Sample_Outputs")
     }
     
     df_name <- deparse(substitute(df))
     output_name <- deparse(substitute(name))
     
-    file_name <- paste("data/", output_name, "_", format(Sys.time(), "%y-%m-%d_%H%M"), 
+    file_name <- paste("../Sample_Outputs/", output_name, "_", format(Sys.time(), "%y-%m-%d_%H%M"), 
                        '.csv', sep = "")
     print(paste("Writing", df_name , "to", file_name))
     write.table(df, file_name, ...)
